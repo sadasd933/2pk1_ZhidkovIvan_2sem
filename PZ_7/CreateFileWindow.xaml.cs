7b9259372a2977b0bbbbdc894a572861ec9c1c9a
qnexc <74954462+sadasd933@users.noreply.github.com>
2022-05-22 14:12:07 +0500

    Add files via upload

---
+++ PZ_7/CreateFileWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PZ_77
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateFileWindow.xaml
+    /// </summary>
+    public partial class CreateFileWindow : Window
+    {
+        public CreateFileWindow()
+        {
+            InitializeComponent();
+        }
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+        public string FileName
+        {
+            get { return fileNameBox.Text; }
+        }
+    }
+}
